49654b186c5ca54e4906075a885d48928d0fa1e7
camilagsoares <camila.soares19@hotmail.com>
2022-02-03 12:38:30 -0300

    Lista criada

--- alura-studies/src/App.tsx
+++ alura-studies/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Form from './components/Form'
+import List from './components/List'
 
 const App = () => {
   return(
     <div>
      <Form />
+     <List />
     </div>
   )
 }
